2be5c5eb528fd4e07f2dab2ff66ab708b1292f6c
sodaakim <sohuikim2020@naver.com>
2023-11-15 15:50:16 +0900

    finish folder

---
+++ Assets/GAME/Scripts/MovingBlock.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class MovingBlock : MonoBehaviour
+{
+
+    // 이동 속도
+    public float moveDistance = 2.0f; // 이동 거리를 조정해보세요.
+
+    private bool isMoving = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 충돌한 오브젝트가 Penguin인지 확인
+        if (collision.gameObject.tag == "Player1")
+        {
+            // Penguin인 경우 움직이지 않음
+            // Debug.Log("Penguin은 움직일 수 없습니다!");
+        }
+        else if (collision.gameObject.tag == "Player2" && !isMoving)
+        {
+            // 충돌 지점의 normal 벡터를 구함
+            ContactPoint contact = collision.contacts[0];
+            Vector3 normal = contact.normal.normalized;
+
+            // y 축에서의 충돌 여부 확인
+            if (Mathf.Abs(normal.y) < 0.5f) // 이 값을 조정해서 위아래 충돌을 판단합니다.
+            {
+                // x, z 축의 비중을 계산하여 더 비중이 큰 축으로 이동 방향 설정
+                Vector3 direction = Vector3.zero;
+                if (Mathf.Abs(normal.x) > Mathf.Abs(normal.z))
+                {
+                    direction = new Vector3(Mathf.Sign(normal.x), 0, 0);
+                }
+                else
+                {
+                    direction = new Vector3(0, 0, Mathf.Sign(normal.z));
+                }
+
+                // 이동 속도와 방향 벡터의 크기를 곱하여 이동 벡터를 얻음
+                Vector3 moveVector = direction * moveDistance;
+
+                // 오브젝트를 부드럽게 이동시킴
+                StartCoroutine(MoveBlockSmoothly(transform.position, transform.position + moveVector, 1.0f));
+            }
+        }
+    }
+
+    // 보간을 통해 부드럽게 이동
+    private System.Collections.IEnumerator MoveBlockSmoothly(Vector3 start, Vector3 end, float timeToMove)
+    {
+        isMoving = true;
+
+        float elapsedTime = 0.0f;
+        while (elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(start, end, elapsedTime / timeToMove);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        isMoving = false;
+    }
+}
---
+++ Assets/GAME/Scripts/Snowman.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowman : MonoBehaviour
+{
+    public List<Transform> targetPositions; // 객체의 이동할 위치 리스트
+
+    private int currentTargetIndex = 0; // 현재 이동할 위치의 인덱스
+
+    public float moveSpeed = 2.5f; // 이동 속도
+
+    void Update()
+    {
+        // 리스트가 비어있거나 유효한 위치가 없으면 종료
+        if (targetPositions == null || targetPositions.Count == 0 || currentTargetIndex >= targetPositions.Count)
+            return;
+
+        // 현재 이동할 위치의 Transform을 가져와 해당 위치로 이동
+        Transform currentTarget = targetPositions[currentTargetIndex];
+        MoveTowardsTarget(currentTarget);
+    }
+
+    void MoveTowardsTarget(Transform target)
+    {
+        // 현재 위치에서 목표 위치로 이동
+        transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+
+        // 만약 현재 위치가 목표 위치에 도달했다면 다음 위치로 인덱스를 이동
+        if (Vector3.Distance(transform.position, target.position) < 0.1f)
+        {
+            currentTargetIndex++;
+            // 이동할 위치가 끝에 도달했을 경우 다시 처음 위치로 돌아가기 위해 인덱스를 초기화
+            if (currentTargetIndex >= targetPositions.Count)
+            {
+                currentTargetIndex = 0;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
+        {
+            GameManager.Instance.GameOver(); // GameManager 스크립트의 GameOver 함수를 호출하는 코드
+        }
+    }
+
+}
