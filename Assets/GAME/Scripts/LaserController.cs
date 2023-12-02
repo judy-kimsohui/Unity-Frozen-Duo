552e370508d8741335a2be2cdfe7705d0caaee0f
박진영 <tear4321@cau.ac.kr>
2023-12-02 20:00:20 +0900

    bugfix/lasercontroller 명시적인 회전 추가

--- Assets/GAME/Scripts/LaserController.cs
+++ Assets/GAME/Scripts/LaserController.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Numerics;
 using Unity.VisualScripting;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 namespace GAME.Scripts
 {
@@ -10,11 +13,10 @@ namespace GAME.Scripts
 
         private bool isHitMeltingIce = false;
         private MeltingIceController currentMeltingIceController = null;
-        
+
         // Start is called before the first frame update
         void Start()
         {
-        
         }
 
         // Update is called once per frame
@@ -37,8 +39,8 @@ namespace GAME.Scripts
             Debug.DrawRay(transform.position, transform.forward, Color.blue, 1f);
             if (Physics.Raycast(new Ray(transform.position, transform.forward), out var hit, MaxLaserLength))
             {
-                if(hit.transform.CompareTag("MeltingIce") && !isHitMeltingIce) HitMeltingIce(hit.transform);
-                else if(!hit.transform.CompareTag("MeltingIce") && isHitMeltingIce) ReleaseMeltingIce();
+                if (hit.transform.CompareTag("MeltingIce") && !isHitMeltingIce) HitMeltingIce(hit.transform);
+                else if (!hit.transform.CompareTag("MeltingIce") && isHitMeltingIce) ReleaseMeltingIce();
                 var scale = laserOffset.transform.localScale;
                 scale.z = hit.distance / 2;
                 laserOffset.transform.localScale = scale;
@@ -48,8 +50,8 @@ namespace GAME.Scripts
                 if (isHitMeltingIce) ReleaseMeltingIce();
                 var scale = laserOffset.transform.localScale;
                 scale.z = MaxLaserLength;
-                laserOffset.transform.localScale = scale; 
-            } 
+                laserOffset.transform.localScale = scale;
+            }
         }
 
         private void HitMeltingIce(Transform transform)
@@ -60,9 +62,40 @@ namespace GAME.Scripts
         }
 
         private void ReleaseMeltingIce()
-        { 
-            if(!currentMeltingIceController.IsDestroyed()) currentMeltingIceController.RemoveHit();
+        {
+            if (!currentMeltingIceController.IsDestroyed()) currentMeltingIceController.RemoveHit();
             isHitMeltingIce = false;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player1") && !other.CompareTag("Player2")) return;
+            
+            var otherPos = other.transform.position;
+            otherPos = otherPos - transform.position;
+            var otherX = Vector3.Dot(otherPos, transform.right);
+            var otherZ = Vector3.Dot(otherPos, transform.forward);
+            var otherPosInLaserCoord = new Vector3(otherX, 0.0f, otherZ);
+            if (IsInFrontOfHandle(otherPosInLaserCoord))
+            {
+                Debug.Log($"in front");
+                this.transform.Rotate(Vector3.up, -15.0f);
+            }
+            else if (IsInBackOfHandle(otherPosInLaserCoord))
+            {
+                Debug.Log($"in back");
+                this.transform.Rotate(Vector3.up, 15.0f);
+            }
+        }
+
+        private bool IsInFrontOfHandle(Vector3 localOtherPos)
+        {
+            return localOtherPos.x < -0.5f && localOtherPos.z > 0.0f;
+        }
+
+        private static bool IsInBackOfHandle(Vector3 localOtherPos)
+        {
+            return localOtherPos.x < -0.5f && localOtherPos.z < 0.0f;
         }
     }
 }
